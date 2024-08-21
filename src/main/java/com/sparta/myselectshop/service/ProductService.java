7030cf113df5de5135cc38c3531084530c32d8f5
kkm4512 <nayoun440@naver.com>
2024-08-21 15:49:13 +0900

    회원별 상품 API 구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -4,6 +4,7 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.entity.User;
 import com.sparta.myselectshop.naver.dtos.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 import jakarta.transaction.Transactional;
@@ -19,14 +20,14 @@ public class ProductService {
     public static final int MIN_MY_PRICE = 100;
 
     @Transactional
-    public ProductResponseDto createProduct(ProductRequestDto requestDto) {
-        Product product = new Product(requestDto);
+    public ProductResponseDto createProduct(ProductRequestDto requestDto, User user) {
+        Product product = new Product(requestDto,user);
         Product saveProduct =  productRepository.save(product);
         return new ProductResponseDto(saveProduct);
     }
 
     @Transactional
-    public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+    public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto, User user) {
         int myprice = requestDto.getMyprice();
         if (myprice < MIN_MY_PRICE) throw new IllegalArgumentException("유효하지않은 관심 가격 입니다 최소 " + MIN_MY_PRICE + "원 이상 입니다");
         Product product = productRepository.findById(id).orElseThrow(() -> new NullPointerException("유효하지 않은 상품 입니다"));
@@ -34,13 +35,18 @@ public class ProductService {
         return new ProductResponseDto(product);
     }
 
-    public List<ProductResponseDto> getProducts() {
-        return productRepository.findAll().stream().map(ProductResponseDto::new).toList();
+    public List<ProductResponseDto> getProducts(User user) {
+        return productRepository.findAllByUser(user).stream().map(ProductResponseDto::new).toList();
     }
 
     @Transactional
     public void updateBySearch(Long id, ItemDto itemDto) {
         Product product = productRepository.findById(id).orElseThrow(() -> new NullPointerException("유효한 상품이 아닙니다"));
         product.updateByItemDto(itemDto);
+    }
+
+    public List<ProductResponseDto> getAllProducts() {
+        return productRepository.findAll().stream().map(ProductResponseDto::new).toList();
+
     }
 }
