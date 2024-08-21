3ac84d9b0a5cebe2b011c8d4c777dec36f71c885
kkm4512 <nayoun440@naver.com>
2024-08-21 14:50:19 +0900

    Scheduler 기능 구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -4,6 +4,7 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.naver.dtos.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -35,5 +36,11 @@ public class ProductService {
 
     public List<ProductResponseDto> getProducts() {
         return productRepository.findAll().stream().map(ProductResponseDto::new).toList();
+    }
+
+    @Transactional
+    public void updateBySearch(Long id, ItemDto itemDto) {
+        Product product = productRepository.findById(id).orElseThrow(() -> new NullPointerException("유효한 상품이 아닙니다"));
+        product.updateByItemDto(itemDto);
     }
 }
