b1af200805e4073ad802d02577ad8f69cef7d4e8
kkm4512 <nayoun440@naver.com>
2024-09-09 17:08:15 +0900

    인가 코드로 access_token 가져오기 & 카카오 사용자 정보 가져오가 구현

--- src/main/java/com/sparta/myselectshop/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/myselectshop/config/WebSecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableWebSecurity // Spring Security 지원을 가능하게 함
